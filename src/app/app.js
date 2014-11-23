329736a5819a1d01b9742fe0d9a5b14e9930962a
Julian Miqueo <kuliantxo@gmail.com>
2014-11-23 13:53:44 -0800

    Nav bar

--- src/app/app.js
+++ src/app/app.js
@@ -21,5 +21,29 @@ angular.module( 'ngBoilerplate', [
   });
 })
 
+.controller( 'PlayerCtrl', function PlayerCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
+    }
+  });
+})
+
+.controller( 'PlayingCtrl', function PlayingCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
+    }
+  });
+})
+
+.controller( 'NavCtrl', function NavCtrl ( $scope, $location ) {
+  $scope.$on('$stateChangeSuccess', function(event, toState, toParams, fromState, fromParams){
+    if ( angular.isDefined( toState.data.pageTitle ) ) {
+      $scope.pageTitle = toState.data.pageTitle + ' | ngBoilerplate' ;
+    }
+  });
+})
+
 ;
 
